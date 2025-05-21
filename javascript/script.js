5b7518f25ea005dcb7cb4f60ef4f8e2e898d6f51
MilaFaleiro <camilagf83@gmail.com>
2025-05-21 19:47:15 +0000

    js

---
+++ javascript/script.js
@@ -0,0 +1,9 @@
+let nome = "Milla";
+let idade = "19";
+let cidade = "Salvador";
+let estado = "Bahia";
+
+console.log("Nome: " + nome);
+console.log("Idade: " + idade);
+console.log("Cidade: " + cidade);
+console.log("Estado: " + estado);
---
+++ javascript/var.js
@@ -0,0 +1,3 @@
+var nome = "Milla";
+
+console.log(`Nome: ${nome}`);
